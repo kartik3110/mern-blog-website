8ef3fc56ce9f98edaad20d902b929841c96de0af
kartik3110 <kartik31.gupta@gmail.com>
2024-02-02 19:04:23 +0530

    added react-router and created routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return <div>hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
