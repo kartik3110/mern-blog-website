6486849357fb944c8cc2c50e1da1776b5b6edfb8
kartik3110 <kartik31.gupta@gmail.com>
2024-02-02 23:06:18 +0530

    created backend server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,9 +12,9 @@ export default function Header() {
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Kartik
+          Blog
         </span>
-        Blogs
+        Hive
       </Link>
       <form>
         <TextInput
@@ -34,6 +34,8 @@ export default function Header() {
         <Link to="/sign-in">
           <Button gradientDuoTone="purpleToBlue">Sign In</Button>
         </Link>
+
+        {/* Hamburger button */}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
